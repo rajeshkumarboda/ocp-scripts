bea0a37c15c715a3213a24b227a2de082ed4b0ed
rajeshkumarboda <rajeshkumarboda@gmail.com>
2021-03-23 23:35:43 +0530

    Update import.sh

--- import.sh
+++ import.sh
@@ -5,26 +5,12 @@ export HOOK_SLEEP=100
 echo 'Downloading SQL script that initializes the database...'
 curl -s -O https://raw.githubusercontent.com/rajeshkumarboda/ocp-scripts/main/users.sql
 echo "Trying $HOOK_RETRIES times, sleeping $HOOK_SLEEP sec between tries:"
-while [ "$HOOK_RETRIES" != 0 ]; do
-echo -n 'Checking if MySQL is up...'
-if mysqlshow -h$MYSQL_SERVICE_HOST -u$MYSQL_USER -P3306
-$MYSQL_DATABASE &>/dev/null
-then
-echo 'Database is up'
-break
-else
-echo 'Database is down'
-# Sleep to wait for the MySQL pod to be ready
-sleep $HOOK_SLEEP
-fi
-let HOOK_RETRIES=HOOK_RETRIES-1
-done
 if [ "$HOOK_RETRIES" = 0 ]; then
 echo 'Too many tries, giving up'
 exit 1
 fi
 # Run the SQL script
-if mysql -h$MYSQL_SERVICE_HOST -u$MYSQL_USER -p$MYSQL_PASSWORD -P3306
+if mysql -h$MYSQL_SERVICE_HOST -u$MYSQL_USER -P3306
 $MYSQL_DATABASE < /tmp/users.sql
 then
 echo 'Database initialized successfully'
